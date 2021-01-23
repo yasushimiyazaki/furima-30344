3c0fc2b463ebb07f362b8611818e9b5957d29ed1
yasushimiyazaki <73190364+yasushimiyazaki@users.noreply.github.com>
2021-01-24 05:16:02 +0900

    ユーザー管理機能修正４

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,15 @@ class User < ApplicationRecord
     validates :first_name_kana
     validates :birthday
     # ひらがな、カタカナ、漢字のみ許可する
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]/, message: 'is invalid. Input full-width characters.' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]/, message: 'is invalid. Input full-width characters.' }
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]/, message: 'is invalid. Input full-width characters.' } do
+    validates :last_name
+    validates :first_name
+    end
     # カタカナのみ許可する
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' } do
+    validates :last_name_kana
+    validates :first_name_kana
+    end
     # 半角英字数字混合のみ許可する
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字混合のみ登録できる。' }
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,32 +1,23 @@
 require 'rails_helper'
 
-FactoryBot.define do
-  factory :user do
-    nickname              { Faker::Name.initials(number: 2) }
-    email                 { 'test@example' }
-    password              { Faker::Internet.password(min_length: 6) }
-    password_confirmation { password }
-    last_name             { '阿阿' }
-    first_name            { '井伊' }
-    last_name_kana        { 'アア' }
-    first_name_kana       { 'イイ' }
-    birthday              { '2000-02-20' }
-  end
-end
-
 RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
   end
 
   describe 'ユーザー新規登録' do
-    context 'うまくいく場合' do
-      it '正常な新規登録' do
-        @user
+    context '新規登録できるとき' do
+      it '全て正しく入力すると登録できる' do
+        expect(@user).to be_valid
+      end
+      it "passwordが6文字以上半角英数字であれば登録できる" do
+        @user.password = "000aaa"
+        @user.password_confirmation = "000aaa"
+        expect(@user).to be_valid
       end
     end
 
-    context 'うまくいかない場合' do
+    context '新規登録出来ないとき' do
       it 'nicknameが空だと登録できない' do
         @user.nickname = ''
         @user.valid?
@@ -41,6 +32,12 @@ RSpec.describe User, type: :model do
         @user.email = 'testexample'
         @user.valid?
         expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+      it "重複したemailが存在する場合登録できない" do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
       end
       it 'passwordが空だと登録できない' do
         @user.password = ''
