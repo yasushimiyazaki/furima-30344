a73b9b206271dacc2695c32d810a04cd071afbcd
yasushimiyazaki <73190364+yasushimiyazaki@users.noreply.github.com>
2021-02-03 14:25:02 +0900

    商品情報編集機能　修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  #before_action :contributor_confirmation, only: [ :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show ]
+  before_action :set_item, only: [:show, :edit, :update ]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -20,16 +20,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user.id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -46,5 +43,7 @@ class ItemsController < ApplicationController
                                  :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
   end
 
- 
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
