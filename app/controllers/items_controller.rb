5b4420fae3448aa4bdc7e2ad3158d2ce3436f1b6
yasushimiyazaki <73190364+yasushimiyazaki@users.noreply.github.com>
2021-02-04 14:53:18 +0900

    修正１
    This reverts commit e06aeb540d17cb02ebc2f7cd76a8070acaafb6c3, reversing
    changes made to 47baaff122e541f2b75c263cdf7830221864855a.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show ]
-  before_action :set_item, only: [:show, :edit, :update ]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
@@ -33,8 +33,14 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
-  
 
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
 
   private
 
