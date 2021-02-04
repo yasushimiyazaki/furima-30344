9da12fcf99ad47848b73bf887db08cd90afbfefb
yasushimiyazaki <73190364+yasushimiyazaki@users.noreply.github.com>
2021-02-04 12:39:43 +0900

    Revert "商品削除機能"
    This reverts commit 92226cd104b8484c6bb5f9c0b70cfbd553b59f7a.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show ]
+  before_action :set_item, only: [:show, :edit, :update ]
 
   def index
-    @items = Item.all.order('created_at DESC')
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
@@ -33,14 +33,8 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+  
 
-  def destroy
-    if @item.destroy
-      redirect_to root_path
-    else
-      redirect_to root_path
-    end
-  end
 
   private
 
